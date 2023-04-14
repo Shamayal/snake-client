5b7a66ae7544e78fbd71a763265bc9f52e7c75a7
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-14 16:36:06 -0400

    WASD Movements

--- input.js
+++ input.js
@@ -1,6 +1,12 @@
+const {connect} = require("./client");
+
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,11 +16,28 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function () {
+const handleUserInput = function (key) {
+  let conn = connection;
   // what happens when "data" is received from stdin, meaning when a key is pressed on keyboard input
-  if (key === '\u0003') {
+  if (key === "\u0003") {
+    console.log("Game Over")
     process.exit();
+  } else if (key === "w") {
+    console.log("Moved up")
+    conn.write("Move: up");
+  } else if (key === "a") {
+    console.log("Moved left")
+    conn.write("Move: left");
+  } else if (key === "s") {
+    console.log("Moved down")
+    conn.write("Move: down");
+  } else if (key === "d") {
+    console.log("Moved right")
+    conn.write("Move: right");
+  } else {
+    console.log("Press a WASD key to move!")
   }
+
 };
 
 module.exports = {setupInput};
