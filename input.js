17bc211c9a1e929667c43348d87fdc777fb7488e
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-14 17:04:57 -0400

    Additional game messages

--- input.js
+++ input.js
@@ -1,11 +1,9 @@
-const {connect} = require("./client");
-
 // Stores the active TCP connection object.
 let connection;
 
 // setup interface to handle user input from stdin
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -16,26 +14,35 @@ const setupInput = function (conn) {
   return stdin;
 };
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   let conn = connection;
   // what happens when "data" is received from stdin, meaning when a key is pressed on keyboard input
   if (key === "\u0003") {
-    console.log("Game Over")
+    console.log("Game Over");
     process.exit();
   } else if (key === "w") {
-    console.log("Moved up")
+    console.log("Moved up");
     conn.write("Move: up");
   } else if (key === "a") {
-    console.log("Moved left")
+    console.log("Moved left");
     conn.write("Move: left");
   } else if (key === "s") {
-    console.log("Moved down")
+    console.log("Moved down");
     conn.write("Move: down");
   } else if (key === "d") {
-    console.log("Moved right")
+    console.log("Moved right");
     conn.write("Move: right");
+  } else if (key === "g") {
+    console.log("Good Game!");
+    conn.write("Say: Good Game!");
+  } else if (key === "b") {
+    console.log("Better luck next time!");
+    conn.write("Say: Better luck next time!");
+  } else if (key === "h") {
+    console.log("Hi! Let's play the snake game!");
+    conn.write("Say: Hi! Let's play the snake game!");
   } else {
-    console.log("Press a WASD key to move!")
+    console.log("Press a WASD key to move!");
   }
 
 };
